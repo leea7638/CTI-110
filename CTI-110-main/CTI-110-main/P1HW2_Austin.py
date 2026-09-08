5727c1a915460f4cff5fd4eff57c1e9f102b59c6
leea7638 <leea7638@student.faytechcc.edu>
2026-09-08 20:40:08 +0000

    P1HW2 part 1

---
+++ CTI-110-main/CTI-110-main/P1HW2_Austin.py
@@ -0,0 +1,40 @@
+# CTI 110
+# P1HW2 - Math
+# Austin Lee
+# 9/8/2026
+# do basic math on numbers
+
+# Ask user to enter their budget
+print("This program calculates and displays travel expenses")
+budget = input("what is your Budget? ")
+
+# Ask user to enter travel destination
+destination = input("Enter your destination: ")
+
+# Ask user for amount they will spend on gas
+Fuel = int(input("how much do you think you will spend on gas? "))
+
+# Ask user for amount they will spend on accommodation
+hotel = int(input("Approximatley, how much will you need for accomodation/hotel? "))
+
+# Ask user for amount they will spend on food
+food = int(input("Lastly, how much do you need for food? "))
+
+# Show location and budget
+print("-----------Travel Expenses-----------")
+print("Location: ", destination)
+print("Initial Budget: ", budget)
+
+# Add expenses
+expenses = Fuel + hotel + food
+print("Fuel:", Fuel)
+print("Accomodation: ", hotel)
+print("Food: ", food)
+print("expenses: ", expenses)
+
+# Subtract expenses from budget
+budget - expenses 
+
+
+# Display results
+print("balance: ", balance)
