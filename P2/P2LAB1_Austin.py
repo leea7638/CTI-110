1ee5a3b2931737486a23bc23bd51e73522f8c662
leea7638 <leea7638@student.faytechcc.edu>
2026-09-15 17:50:32 +0000

    P2LAB1 started

---
+++ P2/P2LAB1_Austin.py
@@ -0,0 +1,21 @@
+"""
+(block comments -- continue until the triple quotes end)
+# CTI 110
+# P2LAB1
+# Austin Lee
+# 9/15/2026
+Get Radius, calculate and display radius, circumference, and area
+"""
+# Input -- get radius
+radius = float(input("what is the radius of the circle? "))
+
+# Calculation -- find diameter, circumference, and area
+# diameter = 2*r, circumference = 2*pi*r, area = pi*r*r
+diameter = 2 * radius
+circumference = 2 * PI * radius
+area = PI * radius * radius
+
+# Output -- .1f, .2f, .3f
+print(f"the diameter is {diameter:.1f}")
+print(f"the circumference is {circumference:.2f}")
+print(f"the radius is {radius:.3f}")
