45a389b1d53c203d67d3a2af9b3d3cef001437bd
leea7638 <leea7638@student.faytechcc.edu>
2026-09-17 18:58:48 +0000

    P2HW2 part 1

---
+++ P2/P2HW2_Austin.py
@@ -0,0 +1,33 @@
+# CTI 110
+# P2HW2 - just the setup
+
+# This example only uses three numbers, the full uses six.
+# get the grades
+grade1 = float(input("Enter grade for module 1: "))
+grade2 = float(input("Enter grade for module 2: "))
+grade3 = float(input("Enter grade for module 3: "))
+grade4 = float(input("Enter grade for module 4: "))
+grade5 = float(input("Enter grade for module 5: "))
+grade6 = float(input("Enter grade for module 6: "))
+
+# put them all into a new list
+grade_list = [grade1, grade2, grade3, grade4, grade5, grade6]
+
+# do some calculations -- minimum, maximum, and average
+min_grade = min(grade_list)
+max_grade = max(grade_list)
+total     = sum(grade_list)
+count     = len(grade_list)
+# TODO: calc average (total / count)
+
+average = total / count
+
+# Print the output
+print("-------------------------------------")
+print(f"Grades: {grade_list}")
+print("----------------Results------------------")
+print(f"{"Lowest Grade:":<15} {min_grade:<15.1f}")
+print(f"{"Highest Grade:":<15} {max_grade:<15.1f}")
+print(f"{"Sum Of Grades:":<15} {total:<15.1f}")
+print(f"{"Average:":<15} {average:<15.2f}")
+print("-------------------------------------")
